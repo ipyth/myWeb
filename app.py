784d2b4da4dd6f930a79d28189191d50af5d903e
ipyth <mylhmail@163.com>
2017-07-05 15:42:00 +0800

    flask路由:     装饰器@app.route()     解析转化器converters     HTTP methods     app.url_map

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    return "Index Page!"
+
+
+@app.route('/hello/')
+def hello():
     return "Hello World!"
 
 
+# converters: any,default,float,int,path,string
+@app.route('/hello/<string:username>')
+def hello_user(username):
+    return "Hello {}".format(username)
+
+
+# HTTP methods: GET,HEAD,POST,PUT,DELETE,OPTIONS
+# default: OPTIONS, GET, HEAD
+@app.route('/users/<string:username>', methods=['GET', 'POST'])
+def users(username):
+    if request.method == 'POST':
+        return "Create user: {}".format(username)
+    else:
+        return "Show user: {}".format(username)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=5000, debug=True)
